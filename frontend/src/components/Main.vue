6b499bfe2c446b1061baf9fc24eb6d17eab22063
USER <father@mail.com>
2020-07-15 10:28:58 +0900

    200715 test

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -5,36 +5,51 @@
         <MainHeader></MainHeader>
         <div class="page-wrap">
           <LSidebar></LSidebar>
-          <div class="main-content" style="padding-bottom:0;"
-               :class="{'disactive-padding': $store.state.selectComponent=='main' }">
-            <div :class="{'row': isVideoMode,'no-gutters':isVideoMode}">
-              <div :class="{'col': isVideoMode,'col-3':isVideoMode}">
-                <NoChannel v-if="$store.state.channelList[0]==null && $store.state.selectComponent=='main'"/>
+          <div
+            class="main-content"
+            style="padding-bottom:0;"
+            :class="{
+              'disactive-padding': $store.state.selectComponent == 'main'
+            }"
+          >
+            <div :class="{ row: isVideoMode, 'no-gutters': isVideoMode }">
+              <div :class="{ col: isVideoMode, 'col-3': isVideoMode }">
+                <NoChannel
+                  v-if="
+                    $store.state.channelList[0] == null &&
+                      $store.state.selectComponent == 'main'
+                  "
+                />
                 <keep-alive v-else>
                   <component :is="whichComponent"></component>
                 </keep-alive>
               </div>
-              <div :class="{'col': isVideoMode,'col-9':isVideoMode}">
-                <VideoChat v-if="$store.state.isVideoMode"/>
+              <div :class="{ col: isVideoMode, 'col-9': isVideoMode }">
+                <VideoChat v-if="$store.state.isVideoMode" />
               </div>
             </div>
-            <RSidebar v-if="$store.state.currentChannel!=null"></RSidebar>
+            <RSidebar v-if="$store.state.currentChannel != null"></RSidebar>
           </div>
           <footer class="footer">
             <div class="w-100 clearfix">
-              <span class="text-center text-sm-left overline d-md-inline-block">Copyright © 2018 ThemeKit</span>
+              <span class="text-center text-sm-left overline d-md-inline-block"
+                >Copyright © 2018 ThemeKit</span
+              >
             </div>
           </footer>
         </div>
       </template>
-      <Loading v-else/>
-
+      <Loading v-else />
     </div>
     <!-- 전체 공지용 스낵바 modal -->
-    <v-snackbar v-model="noticeMsgToggle" :timeout='5000' :top="true"
-                style="margin-top: 8vh; font-size: medium;"
-                color="#404E67">
-      {{noticeMsg}}
+    <v-snackbar
+      v-model="noticeMsgToggle"
+      :timeout="5000"
+      :top="true"
+      style="margin-top: 8vh; font-size: medium;"
+      color="#404E67"
+    >
+      {{ noticeMsg }}
       <v-btn icon color="white" @click="noticeMsgToggle = false">
         <v-icon>close</v-icon>
       </v-btn>
@@ -42,133 +57,134 @@
   </div>
 </template>
 <script>
-  import LSidebar from '../views/main/LSidebar'
-  import RSidebar from '../views/main/RSidebar'
-  import MainHeader from '../views/main/MainHeader'
-  import ContentWrapper from '../views/main/ContentWrapper'
-  import AboutChannel from '../service/aboutchannel'
-  import NotificationClass from '../service/notification'
-  import EventListener from '../service/eventlistener'
-  import UserInfo from "../views/user/UserInfo"
-  import EditProfile from "../views/user/EditProfile"
-  import ChannelHeader from "../views/main/ChannelHeader"
-  import NoChannel from '../views/main/NoChannel'
-  import Loading from '../views/main/Loading'
-  import Stomp from "webstomp-client";
-  import SockJS from "sockjs-client";
-  import TodoList from '../views/todolist/TodoList'
-  import Calendar from "../views/calendar/Calendar";
-  import AdminPage from "../views/admin/AdminPage"
-  import VideoChat from "./VideoChat";
-  import FileDrawer from "./FileDrawer";
-  import Dashboard from "./Dashboard";
+import LSidebar from "../views/main/LSidebar";
+import RSidebar from "../views/main/RSidebar";
+import MainHeader from "../views/main/MainHeader";
+import ContentWrapper from "../views/main/ContentWrapper";
+import AboutChannel from "../service/aboutchannel";
+import NotificationClass from "../service/notification";
+import EventListener from "../service/eventlistener";
+import UserInfo from "../views/user/UserInfo";
+import EditProfile from "../views/user/EditProfile";
+import ChannelHeader from "../views/main/ChannelHeader";
+import NoChannel from "../views/main/NoChannel";
+import Loading from "../views/main/Loading";
+import Stomp from "webstomp-client";
+import SockJS from "sockjs-client";
+import TodoList from "../views/todolist/TodoList";
+import Calendar from "../views/calendar/Calendar";
+import AdminPage from "../views/admin/AdminPage";
+import VideoChat from "./VideoChat";
+import FileDrawer from "./FileDrawer";
 
-  export default {
-    name: 'Main',
-    components: {
-      'MainHeader': MainHeader,
-      'LSidebar': LSidebar,
-      'RSidebar': RSidebar,
-      'ChannelHeader': ChannelHeader,
-      'ContentWrapper': ContentWrapper,
-      'UserInfo': UserInfo,
-      'EditProfile': EditProfile,
-      'NoChannel': NoChannel,
-      'Loading': Loading,
-      'TodoList': TodoList,
-      'Calendar': Calendar,
-      'AdminPage': AdminPage,
-      'VideoChat': VideoChat,
-      'FileDrawer': FileDrawer,
-      'Dashboard': Dashboard
-    },
-    data() {
-      return {
-        noticeMsgToggle: false,
-        noticeMsg: '',
-        channelTitle: '',
-        isRActive: false,
-        modalObj: {modalTitle: '', currentChannel: null},
+export default {
+  name: "Main",
+  components: {
+    MainHeader: MainHeader,
+    LSidebar: LSidebar,
+    RSidebar: RSidebar,
+    ChannelHeader: ChannelHeader,
+    ContentWrapper: ContentWrapper,
+    UserInfo: UserInfo,
+    EditProfile: EditProfile,
+    NoChannel: NoChannel,
+    Loading: Loading,
+    TodoList: TodoList,
+    Calendar: Calendar,
+    AdminPage: AdminPage,
+    VideoChat: VideoChat,
+    FileDrawer: FileDrawer
+  },
+  data() {
+    return {
+      noticeMsgToggle: false,
+      noticeMsg: "",
+      channelTitle: "",
+      isRActive: false,
+      modalObj: { modalTitle: "", currentChannel: null }
+    };
+  },
+  computed: {
+    whichComponent() {
+      AboutChannel.updateLastAccessStatus(
+        this.$store.state.oldComponent,
+        this.$store.state.selectComponent
+      );
+      switch (this.$store.state.selectComponent) {
+        case "main":
+          this.$store.commit("setIsVideoMode", false);
+          return "ContentWrapper";
+        case "user":
+          return "UserInfo";
+        case "edit":
+          return "EditProfile";
+        case "todoList":
+          return "TodoList";
+        case "calendar":
+          return "Calendar";
+        case "admin":
+          return "AdminPage";
+        case "fileDrawer":
+          return "FileDrawer";
+        // case 'videoChat':
+        //   return 'VideoChat'
+        case "dash":
+          return "Dashboard";
+        default:
+          return "ContentWrapper";
       }
     },
-    computed: {
-      whichComponent() {
-        AboutChannel.updateLastAccessStatus(this.$store.state.oldComponent, this.$store.state.selectComponent)
-        switch (this.$store.state.selectComponent) {
-          case 'main':
-            this.$store.commit('setIsVideoMode',false)
-            return 'ContentWrapper'
-          case 'user':
-            return 'UserInfo'
-          case 'edit':
-            return 'EditProfile'
-          case 'todoList':
-            return 'TodoList'
-          case 'calendar':
-            return 'Calendar'
-          case 'admin':
-            return 'AdminPage'
-          case 'fileDrawer':
-            return 'FileDrawer'
-          // case 'videoChat':
-          //   return 'VideoChat'
-          case "dash":
-            return "Dashboard";
-          default:
-            return 'ContentWrapper'
-        }
-      },
-      connectionCheck() {
-        if (this.$store.state.stompClient != null) {
-          return this.$store.state.stompClient.connected
-        }
-      },
-    },
-    deactivated() {
-    },
-    async created() {
-      //await this.$store.dispatch('userListUpdate')
-      this.selectChannelList("dash");
-      const currentChannel = this.$store.state.currentChannel
-      if (currentChannel != null) {
-        currentChannel.count = 0
+    connectionCheck() {
+      if (this.$store.state.stompClient != null) {
+        return this.$store.state.stompClient.connected;
       }
-      this.connect()
-      EventListener.resizeEvt()
-      EventListener.beforeunloadEvt()
-      EventListener.focusEvt(this)
-      EventListener.blurEvt()
-      NotificationClass.requestPermission()
-      this.$store.commit('setSmallWidth', (window.innerWidth < 600) ? true : false)
-    },
-    updated() {
-    },
-    methods: {
-      connect: function () {
-        // 새로고침 했을때 Main의 로직이 실행되지 않는 환경에서는 문제가 생길 수 있음
-        this.$store.state.stompClient = Stomp.over(new SockJS('/endpoint/'))
-        this.$store.state.stompClient.connect(this.$store.state.currentUser, () => {
-          this.selectChannelList()
+    }
+  },
+  deactivated() {},
+  async created() {
+    //await this.$store.dispatch('userListUpdate')
+    const currentChannel = this.$store.state.currentChannel;
+    if (currentChannel != null) {
+      currentChannel.count = 0;
+    }
+    this.connect();
+    EventListener.resizeEvt();
+    EventListener.beforeunloadEvt();
+    EventListener.focusEvt(this);
+    EventListener.blurEvt();
+    NotificationClass.requestPermission();
+    this.$store.commit("setSmallWidth", window.innerWidth < 600 ? true : false);
+  },
+  updated() {},
+  methods: {
+    connect: function() {
+      // 새로고침 했을때 Main의 로직이 실행되지 않는 환경에서는 문제가 생길 수 있음
+      this.$store.state.stompClient = Stomp.over(new SockJS("/endpoint/"));
+      this.$store.state.stompClient.connect(
+        this.$store.state.currentUser,
+        () => {
+          this.selectChannelList();
           this.subscribe("/sub/sync/info", res => {
             if (res.headers.noticeMsg != null) {
-              this.noticeMsg = res.headers.noticeMsg
-              this.noticeMsgToggle = true
+              this.noticeMsg = res.headers.noticeMsg;
+              this.noticeMsgToggle = true;
             }
-          })
-        }, (e) => {
+          });
+        },
+        e => {
           if (!this.$store.state.isLogout) {
             // window.location.href = "/"
           }
-        })
-      },
+        }
+      );
     }
   }
+};
 </script>
 <style>
-  .disactive-padding {
-    padding-top: 0 !important;
-    padding-right: 0 !important;
-    padding-left: 240px !important;
-
-  }
+.disactive-padding {
+  padding-top: 0 !important;
+  padding-right: 0 !important;
+  padding-left: 240px !important;
+}
 </style>
--- frontend/src/views/main/MsgBox.vue
+++ frontend/src/views/main/MsgBox.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
     <li class="list-unstyled chat-message" v-if="!isMsgByLoginUser">
-      <div class="myflex" @mouseover="showMsgOption(msg.id)" @mouseleave="hideMsgOption(msg.id)">
+      <div
+        class="myflex"
+        @mouseover="showMsgOption(msg.id)"
+        @mouseleave="hideMsgOption(msg.id)"
+      >
         <div class="icon">
           <slot name="m-icon">
-            <img class="icon-round" :src="msg.user.picture" width="40" height="40"/>
+            <img
+              class="icon-round"
+              :src="msg.user.picture"
+              width="40"
+              height="40"
+            />
           </slot>
         </div>
         <!-- flex에서 벗어나기 위해 감쌈  -->
@@ -17,31 +26,51 @@
           <!-- 채팅메세지내용 -->
           <div style="display:flex;">
             <slot name="m-content">
-              <div v-if="checkMsgType" v-html="textbyFilter(msg.content)"
-                   class="mychat-content"></div>
+              <div
+                v-if="checkMsgType"
+                v-html="textbyFilter(msg.content)"
+                class="mychat-content"
+              ></div>
               <div style="display:flex;align-items: flex-end;">
                 <div v-if="checkFileType" class="mychat-content">
                   <b-row>
-                    <b-col v-for="(file,index) in msg.files" :key="index">
+                    <b-col v-for="(file, index) in msg.files" :key="index">
                       <a @click="fileDownload(file)">
                         <div class="hori-align">
-                          <b-img :alt="file.original_name" :src="selectImage(file)" @load="$emit('imgLoad')"
-                                 style="max-width:100px"></b-img>
+                          <b-img
+                            :alt="file.original_name"
+                            :src="selectImage(file)"
+                            @load="$emit('imgLoad')"
+                            style="max-width:100px"
+                          ></b-img>
                         </div>
-                        <p class="file-name"><b>{{file.original_name}}</b></p>
-                        <p style="margin:0px;">file size : {{(file.file_size /
-                          1024).toLocaleString(undefined,{minimumFractionDigits:2})}}
-                          kb</p>
+                        <p class="file-name">
+                          <b>{{ file.original_name }}</b>
+                        </p>
+                        <p style="margin:0px;">
+                          file size :
+                          {{
+                            (file.file_size / 1024).toLocaleString(undefined, {
+                              minimumFractionDigits: 2
+                            })
+                          }}
+                          kb
+                        </p>
                       </a>
                     </b-col>
                   </b-row>
                 </div>
-                <span style="font-size: 11px; margin:0px 3px; width:53px; ">{{ msg.str_send_date }}</span>
-                <a class="verti-align confirmMsgDel" :id="'confirmMsgDel' + msg.id" @click="confirmDelete(msg)">
+                <span style="font-size: 11px; margin:0px 3px; width:53px; ">{{
+                  msg.str_send_date
+                }}</span>
+                <a
+                  class="verti-align confirmMsgDel"
+                  :id="'confirmMsgDel' + msg.id"
+                  @click="confirmDelete(msg)"
+                >
                   <v-icon style="font-size:16px;">delete_outline</v-icon>
                 </a>
               </div>
-
             </slot>
           </div>
           <!-- 채팅메시지내용끝 -->
@@ -51,30 +80,48 @@
 
     <li class="list-unstyled chat-message msgflex-end" v-else>
       <!-- flex에서 벗어나기 위해 감쌈  -->
-      <div @mouseover="showMsgOption(msg.id)" @mouseleave="hideMsgOption(msg.id)">
+      <div
+        @mouseover="showMsgOption(msg.id)"
+        @mouseleave="hideMsgOption(msg.id)"
+      >
         <!-- 채팅메세지내용 -->
         <div style="display:flex;">
           <slot name="m-content">
             <div style="display:flex;align-items: flex-end;">
-              <a class="verti-align confirmMsgDel" :id="'confirmMsgDel' + msg.id" @click="confirmDelete(msg)">
+              <a
+                class="verti-align confirmMsgDel"
+                :id="'confirmMsgDel' + msg.id"
+                @click="confirmDelete(msg)"
+              >
                 <v-icon style="font-size:16px;">delete_outline</v-icon>
               </a>
-              <span style="font-size: 11px; margin:0px 3px; width:53px; ">{{ msg.str_send_date }}</span>
+              <span style="font-size: 11px; margin:0px 3px; width:53px; ">{{
+                msg.str_send_date
+              }}</span>
             </div>
-            <div v-if="checkMsgType" v-html="textbyFilter(msg.content)"
-                 class="my-message mychat-content"></div>
+            <div
+              v-if="checkMsgType"
+              v-html="textbyFilter(msg.content)"
+              class="my-message mychat-content"
+            ></div>
             <div v-if="checkFileType" class="my-message mychat-content">
               <b-row>
-                <b-col v-for="(file,index) in msg.files" :key="index">
+                <b-col v-for="(file, index) in msg.files" :key="index">
                   <a @click="fileDownload(file)">
                     <div class="hori-align">
-                      <b-img :alt="file.original_name" :src="selectImage(file)" @load="$emit('imgLoad')"
-                             style="max-width:100px"></b-img>
+                      <b-img
+                        :alt="file.original_name"
+                        :src="selectImage(file)"
+                        @load="$emit('imgLoad')"
+                        style="max-width:100px"
+                      ></b-img>
                     </div>
                     <!-- <b-img thumbnail rounded fluid  alt="이미지를 찾을 수 없습니다."
                            style="max-width: 200px" ></b-img> -->
-                    <p class="file-name"><b>{{file.original_name}}</b></p>
-                    <p style="margin:0px;">{{formatBytes(file.file_size)}}</p>
+                    <p class="file-name">
+                      <b>{{ file.original_name }}</b>
+                    </p>
+                    <p style="margin:0px;">{{ formatBytes(file.file_size) }}</p>
                   </a>
                 </b-col>
               </b-row>
@@ -87,87 +134,102 @@
   </div>
 </template>
 <script>
-  import CommonClass from "../../service/common";
+import CommonClass from "../../service/common";
 
-  export default {
-    name: 'MsgBox',
-    props: ['msg'],
-    data() {
-      return {
-        isMsgOption: false
+export default {
+  name: "MsgBox",
+  props: ["msg"],
+  data() {
+    return {
+      isMsgOption: false
+    };
+  },
+  computed: {
+    isMsgByLoginUser: function() {
+      return this.msg.sender == this.currentUser.email;
+    },
+    checkMsgType: function() {
+      return this.msg.message_type == "message" || this.msg.delete_yn == "Y";
+    },
+    checkFileType: function() {
+      return this.msg.message_type == "file" && this.msg.delete_yn == "N";
+    }
+  },
+  methods: {
+    formatBytes: function(byte) {
+      return CommonClass.formatBytes(byte);
+    },
+    showMsgOption: function(msgId) {
+      if (
+        this.msg.delete_yn === "N" &&
+        (this.isMsgByLoginUser || this.isAdmin())
+      ) {
+        this.hideMsgOption();
+        $("#confirmMsgDel" + msgId).css("visibility", "visible");
       }
     },
-    computed: {
-      isMsgByLoginUser: function () {
-        return this.msg.sender == this.currentUser.email
-      },
-      checkMsgType: function () {
-        return this.msg.message_type == 'message' || this.msg.delete_yn == 'Y'
-      },
-      checkFileType: function () {
-        return this.msg.message_type == 'file' && this.msg.delete_yn == 'N'
-      },
+    hideMsgOption: function(msgId) {
+      $(".confirmMsgDel").css("visibility", "hidden");
     },
-    methods: {
-      formatBytes: function (byte) {
-        return CommonClass.formatBytes(byte)
-      },
-      showMsgOption: function (msgId) {
-        if (this.msg.delete_yn === 'N' && (this.isMsgByLoginUser || this.isAdmin())) {
-          this.hideMsgOption()
-          $("#confirmMsgDel" + msgId).css("visibility", "visible")
-        }
-      },
-      hideMsgOption: function (msgId) {
-        $(".confirmMsgDel").css("visibility", "hidden")
-      },
-      textbyFilter: function (content) {
-        const tagContentRegexp = new RegExp(/<p(.*?)>(.*?)<\/p>/g);
-        const htmlTagRegexp = new RegExp(/(<([^>]+)>)/ig);
-        const urlRegexp = new RegExp(/(http(s)?:\/\/|www.)([a-z0-9\w]+\.*)+[a-z0-9]{2,4}([\/a-z0-9-%#?&=\w])+(\.[a-z0-9]{2,4}(\?[\/a-z0-9-%#?&=\w]+)*)*/)
-        let result = '';
-        if (this.$store.state.searchText == '') {
-          content.match(tagContentRegexp).forEach(contentItem => {
-            if (urlRegexp.test(contentItem)) {
-              contentItem = contentItem.replace(htmlTagRegexp, '')
-              result += "<p><a style='color: blue' href='" + contentItem + "' target='_blank'>" + contentItem + "</a></p>"
-            } else {
-              result += contentItem
-            }
-          });
-          return result
-        }
-        return this.$options.filters.highlight(content, this.$store.state.searchText);
-      },
-      selectImage: function (file) {
-        return CommonClass.checkFileType(file)
-      },
-      fileDownload: function (file) {
-        this.$http.get("/api/file/download/" + file.server_name, {
-          responseType: 'blob'
+    textbyFilter: function(content) {
+      console.log(this);
+      const tagContentRegexp = new RegExp(/<p(.*?)>(.*?)<\/p>/g);
+      const htmlTagRegexp = new RegExp(/(<([^>]+)>)/gi);
+      const urlRegexp = new RegExp(
+        /(http(s)?:\/\/|www.)([a-z0-9\w]+\.*)+[a-z0-9]{2,4}([\/a-z0-9-%#?&=\w])+(\.[a-z0-9]{2,4}(\?[\/a-z0-9-%#?&=\w]+)*)*/
+      );
+      let result = "";
+      if (this.$store.state.searchText == "") {
+        content.match(tagContentRegexp).forEach(contentItem => {
+          if (urlRegexp.test(contentItem)) {
+            contentItem = contentItem.replace(htmlTagRegexp, "");
+            result +=
+              "<p><a style='color: blue' href='" +
+              contentItem +
+              "' target='_blank'>" +
+              contentItem +
+              "</a></p>";
+          } else {
+            result += contentItem;
+          }
+        });
+        return result;
+      }
+      return this.$options.filters.highlight(
+        content,
+        this.$store.state.searchText
+      );
+    },
+    selectImage: function(file) {
+      return CommonClass.checkFileType(file);
+    },
+    fileDownload: function(file) {
+      this.$http
+        .get("/api/file/download/" + file.server_name, {
+          responseType: "blob"
         })
-          .then(res => {
-            const url = window.URL.createObjectURL(new Blob([res.data]))
-            const link = document.createElement('a')
-            link.href = url;
-            link.setAttribute('download', file.original_name)
-            document.body.appendChild(link)
-            link.click()
-            link.remove()
-            window.URL.revokeObjectURL(url)
-          })
-      },
-    },
-    filters: {
-      highlight: function (stringToSearch, searchTerm) {
-        if (searchTerm === "") return stringToSearch;
-        var iQuery = new RegExp(searchTerm, "ig");
-        return stringToSearch
-          .toString()
-          .replace(iQuery, function (matchedText, a, b) {
-            return "<span class='highlight'>" + matchedText + "</span>";
-          });
-      }
+        .then(res => {
+          const url = window.URL.createObjectURL(new Blob([res.data]));
+          const link = document.createElement("a");
+          link.href = url;
+          link.setAttribute("download", file.original_name);
+          document.body.appendChild(link);
+          link.click();
+          link.remove();
+          window.URL.revokeObjectURL(url);
+        });
+    }
+  },
+  filters: {
+    highlight: function(stringToSearch, searchTerm) {
+      if (searchTerm === "") return stringToSearch;
+      var iQuery = new RegExp(searchTerm, "ig");
+      return stringToSearch
+        .toString()
+        .replace(iQuery, function(matchedText, a, b) {
+          return "<span class='highlight'>" + matchedText + "</span>";
+        });
     }
   }
+};
 </script>
